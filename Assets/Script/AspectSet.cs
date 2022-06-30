391f15b6183de4f3feb06c1f3cdcde271707fd49
ibelieveinrustsuperiority <20221022@vision.hoseo.edu>
2022-06-30 16:24:39 +0900

    chore: cleanup code

--- Assets/Script/AspectSet.cs
+++ Assets/Script/AspectSet.cs
@@ -1,59 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AspectSet : MonoBehaviour
 {
     public Dropdown resolutionsDropdown;
-    List<Resolution> resolutions = new List<Resolution>();
-    public int resolutionNum;
     public Toggle fullscreenBtn;
-    int optionNum = 0;
-    FullScreenMode screenMode;
+
+    readonly List<Resolution> _resolutions = new();
+    FullScreenMode _screenMode;
+    
     void Start()
     {
-        initUI();
+        InitUI();
     }
 
     
     void Update()
     {
-        exitSetting();
-    }
-    void initUI () {
-        for (int i = 0; i < Screen.resolutions.Length; i++) {
-            if (Screen.resolutions[i].refreshRate == 60)
-                resolutions.Add(Screen.resolutions[i]);
-        }
-        resolutionsDropdown.options.Clear();
-        foreach (Resolution item in resolutions) {
-            Dropdown.OptionData option = new Dropdown.OptionData();
-            option.text = item.width + "x" + item.height;
-            resolutionsDropdown.options.Add(option);
-            if(item.width == Screen.width && item.height == Screen.height) {
-                resolutionsDropdown.value = optionNum;
-            }
-            optionNum++;
-        }
-        resolutionsDropdown.RefreshShownValue();
-        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
-    }
-    public void DropdownOptionChange (int x) {
-        resolutionNum = x;
-    }
-    public void ApplyButton () {
-        Screen.SetResolution(resolutions[resolutionNum].width, 
-            resolutions[resolutionNum].height,
-            screenMode);
-    }
-    public void FullScreenBtn (bool isFull) {
-        screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
-    }
-    void exitSetting () {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             //Instantiate();//return to menu prefab
             Destroy(gameObject);
         }
+    }
+    
+    void InitUI () {
+        foreach (var resolution in Screen.resolutions)
+        {
+            if (resolution.refreshRate == 60) // 항상 60hz만 서포트?
+                _resolutions.Add(resolution);
+        }
+        
+        resolutionsDropdown.options.Clear();
+
+        // Add all available resolutions
+        resolutionsDropdown.AddOptions(_resolutions.Select(res => res.width + "x" + res.height).ToList());
+
+        // Update the dropdown appearance
+        resolutionsDropdown.RefreshShownValue();
+        fullscreenBtn.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow);
+    }
+    
+    // Used in inspector
+    public void ApplyButton () {
+        Screen.SetResolution(_resolutions[resolutionsDropdown.value].width, 
+            _resolutions[resolutionsDropdown.value].height,
+            _screenMode);
+    }
+    
+    // Used in inspector
+    public void FullScreenBtn (bool isFull) {
+        _screenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
     }
 }
