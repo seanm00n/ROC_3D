982031f01da3eb6c36fe0151214b4f78c4099d33
seanm00n <deccj97@naver.com>
2022-07-04 19:29:14 +0900

    feat MonsterAI 0704

--- Assets/Script/MonsterAI.cs
+++ Assets/Script/MonsterAI.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class MonsterAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    //Do not change values with inspector
+    int healhPoint = 1;
+    int attackPoint = 1;
+    int speedPoint = 1;
+    int abilityConst = 2; //Ability constant per stage
+    public int stage = 1;
+    public bool isBoss = false;
+
+    void Start(){
+        InitMonster();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        CheckDeath();
+        MonsterBehavior();
+    }
+
+    void InitMonster () {
+        healhPoint = stage * abilityConst;
+        attackPoint = stage * abilityConst;
+        speedPoint = stage * abilityConst;
+    }
+
+    void MonsterBehavior () {
+        //get target
+
+        //rotate to target
+
+        //move to target
+        GetComponent<Animator>().SetBool("Run", true);//"Run" 수정 필요
+
+        //avoid obstacles
+
+        //attack target
+        GetComponent<Animator>().SetBool("Attack", true);//"Attack"수정 필요
+
+    }
+
+    void CheckDeath () {
+        if (healhPoint <= 0) {
+            GetComponent<Animator>().SetBool("Death", true);//"Death" 수정 필요
+            StartCoroutine("DestroyDelay");
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator DestroyDelay () {
+        yield return new WaitForSeconds(3f);
     }
 }
