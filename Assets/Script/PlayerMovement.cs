8c5e4f3e98dc3db0bf6fc5094327008cbd946a9a
Zinramen <fhgh6290@gmail.com>
2022-07-06 20:04:39 +0900

    Player 관성 새로 제작
    1. 관성 작동 방식이 변경됨.
    (관성의 반대 방향으로 힘이 작동하면
    결국 멈추게 됨.)

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float airSpeedX;
+    float airSpeedZ;
+
     bool sitContinue = false;
     public bool waitingAnimation = false;
     public bool DoJump = false;
@@ -18,12 +21,10 @@ public class PlayerMovement : MonoBehaviour
     public float stopSpeed;
 
     [SerializeField] float jumpSpeed = 0.08f;
+    [SerializeField] float SmoothStopIntensity = 0.05f;
     public bool isJumping = false;
 
     [SerializeField] float levitateMinusTime = 0.06f;
-
-    float airSpeed_x = 0;
-    float airSpeed_z = 0;
 
     public float Anti_Inertia = 8;
 
@@ -49,13 +50,56 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (airSpeed_x > 0) airSpeed_x -= Time.deltaTime; else if (airSpeed_x < 0) airSpeed_x += Time.deltaTime; 
-
-        if (airSpeed_z > 0) airSpeed_z -= Time.deltaTime; else if (airSpeed_z < 0) airSpeed_z += Time.deltaTime;
 
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         float verticalMove = Input.GetAxisRaw("Vertical");
 
+        if (airSpeedX > 0)
+        {
+            airSpeedX -= airSpeedX * Time.unscaledDeltaTime;
+        }
+        else if (airSpeedX < 0)
+        {
+            airSpeedX += airSpeedX * Time.unscaledDeltaTime;
+        }
+        else { airSpeedX = 0; }
+
+        if (airSpeedZ > 0)
+        {
+            airSpeedZ -= airSpeedZ * Time.unscaledDeltaTime;
+        }
+        else if (airSpeedZ < 0)
+        {
+            airSpeedX += airSpeedX * Time.unscaledDeltaTime;
+        }
+        else { airSpeedZ = 0; }
+
+        ///////////////////////////////////////////////////////////////
+
+        if ((airSpeedX) > 0) 
+        {
+            airSpeedX += horizontalMove * SmoothStopIntensity* Time.unscaledDeltaTime;
+            if (airSpeedX < 0) airSpeedX = 0;
+        }
+        else if ((airSpeedX) < 0)
+        {
+            airSpeedX += horizontalMove * SmoothStopIntensity * Time.unscaledDeltaTime;
+            if (airSpeedX > 0) airSpeedX = 0;
+        }
+
+        if ((airSpeedZ) > 0)
+        {
+            airSpeedZ += verticalMove * SmoothStopIntensity * Time.unscaledDeltaTime; 
+            if (airSpeedZ < 0) airSpeedZ = 0;
+        }
+        else if ((airSpeedZ) < 0)
+        {
+            airSpeedZ += verticalMove * SmoothStopIntensity * Time.unscaledDeltaTime; 
+            if (airSpeedZ > 0) airSpeedZ = 0;
+        }
+
+        /////////////////////////////////////////////////////////////////
+        
         ChMove(horizontalMove, verticalMove);
         ChJump(horizontalMove, verticalMove);
     }
@@ -64,46 +108,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (gravity_Max > gravity && isJumping == false)
         {
-            gravity += gravitySpeed * Time.deltaTime;
+            gravity += gravitySpeed * Time.unscaledDeltaTime;
         }
-        if (currenJumpSpeed > 0) currenJumpSpeed -= levitateMinusTime * Time.deltaTime; else currenJumpSpeed = 0;
+        if (currenJumpSpeed > 0) currenJumpSpeed -= levitateMinusTime * Time.unscaledDeltaTime; else currenJumpSpeed = 0;
 
-        if (Input.GetKeyDown(KeyCode.C)) 
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            if (sitContinue) { sitContinue = false; } else { sitContinue = true; } 
-        } 
+            if (sitContinue) { sitContinue = false; } else { sitContinue = true; }
+        }
         if (((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) || sitContinue == true) && isJumping == true) speed = Slowspeed;
         else if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && isJumping == true) speed = Dashspeed;
-        else if(isJumping == true)speed = OriginalSpeed;
+        else if (isJumping == true) speed = OriginalSpeed;
 
-        float moveX, moveZ;
+        float moveX = 0, moveZ = 0;
 
-        if (airSpeed_x > 0)
+        if (airSpeedX != 0)
+            moveX = (airSpeedX);
+        else if (isJumping == true || speed != Dashspeed) { moveX = (horizontalMove); }
 
-            moveX = airSpeed_x - (horizontalMove * stopSpeed) * Time.deltaTime;
-        
-        else if (airSpeed_x < 0)
-
-            moveX = airSpeed_x + (horizontalMove * stopSpeed) * Time.deltaTime;
-
-        else
-            moveX = (horizontalMove);
-        
+        if (airSpeedZ != 0)
+            moveZ = (airSpeedZ);
+        else if (isJumping == true || speed != Dashspeed) {moveZ = (verticalMove); }
         ////////////////////////////////////
         ///
-
-        if (airSpeed_z > 0)
-
-            moveZ = airSpeed_z - (verticalMove * stopSpeed) * Time.deltaTime;
-
-        else if (airSpeed_z < 0)
-
-            moveZ = airSpeed_z + (verticalMove * stopSpeed) * Time.deltaTime;
-
-        else
-            moveZ = (verticalMove);
-
-
 
         if (speed > OriginalSpeed) PlayerAnimControl.instance.Player.SetBool("Dash", true); 
         else PlayerAnimControl.instance.Player.SetBool("Dash", false);
@@ -113,10 +140,10 @@ public class PlayerMovement : MonoBehaviour
 
         PlayerAnimControl.instance.AnimationWork(new Vector2(horizontalMove, verticalMove));
 
-        Vector3 myVelocity = Vector3.Normalize(transform.right * moveX) * speed * Time.deltaTime;
-        myVelocity += Vector3.Normalize(transform.forward * moveZ) * speed * Time.deltaTime;
+        Vector3 myVelocity = Vector3.Normalize(transform.right * moveX) * speed;
+        myVelocity += Vector3.Normalize(transform.forward * moveZ) * speed;
 
-        mybody.Move(new Vector3(myVelocity.x, (-gravity + currenJumpSpeed), myVelocity.z) * 200 * Time.deltaTime); 
+        mybody.Move(new Vector3(myVelocity.x, (-gravity + currenJumpSpeed), myVelocity.z) * 200 * Time.unscaledDeltaTime); 
 
     }
 
@@ -125,7 +152,7 @@ public class PlayerMovement : MonoBehaviour
         Debug.DrawRay(transform.position, -transform.up * (mybody.height / (mybody.height * 2)), Color.green);
         if (Physics.Raycast(transform.position, -transform.up, (mybody.height / (mybody.height * 2)), ~playerLayer) || mybody.isGrounded)
         {
-            if (isJumping == false) { airSpeed_x = 0; airSpeed_z = 0; currenJumpSpeed = 0; isJumping = true; gravity = gravity_early; }
+            if (isJumping == false) { currenJumpSpeed = 0; isJumping = true; gravity = gravity_early; airSpeedX = 0; airSpeedZ = 0; }
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -134,17 +161,18 @@ public class PlayerMovement : MonoBehaviour
                 PlayerAnimControl.instance.Jump();
                 var velocity = new Vector3(horizontalMove * speed, jumpSpeed , verticalMove * speed);
                 currenJumpSpeed = jumpSpeed;
-                mybody.Move(velocity * Time.deltaTime);
+                mybody.Move(velocity * Time.unscaledDeltaTime);
 
                 if (speed == Dashspeed)
                 {
-                    airSpeed_x = (int)mybody.velocity.x / Anti_Inertia;
-                    airSpeed_z = (int)mybody.velocity.z / Anti_Inertia;
+                    airSpeedX = horizontalMove * speed / Anti_Inertia;
+                    airSpeedZ = verticalMove * speed / Anti_Inertia;
+
                 }
                 else
                 {
-                    airSpeed_x = 0;
-                    airSpeed_z = 0;
+                    airSpeedX = 0;
+                    airSpeedZ = 0;
 
                 }
             }
