82092660fc98d50574f2216f85e3fdb1f8bb8fff
seanm00n <deccj97@naver.com>
2022-07-06 18:22:39 +0900

    feat MonsterAI 0706

--- Assets/Script/MonsterAI.cs
+++ Assets/Script/MonsterAI.cs
@@ -7,18 +7,23 @@ public class MonsterAI : MonoBehaviour
     //Do not change values with inspector
     int healhPoint = 1;
     int attackPoint = 1;
-    int speedPoint = 1;
+    float speedPoint = 100;
     int abilityConst = 2; //Ability constant per stage
+    Vector3 target;
+    Vector3 force;
     public int stage = 1;
     public bool isBoss = false;
+
 
     void Start(){
         InitMonster();
     }
 
     void Update(){
-        CheckDeath();
+        //CheckDeath();
         MonsterBehavior();
+        //rotate to target
+        //avoid obstacles
     }
 
     void InitMonster () {
@@ -28,23 +33,30 @@ public class MonsterAI : MonoBehaviour
     }
 
     void MonsterBehavior () {
-        //get target
-
-        //rotate to target
-
-        //move to target
-        GetComponent<Animator>().SetBool("Run", true);//"Run" 수정 필요
-
-        //avoid obstacles
-
-        //attack target
-        GetComponent<Animator>().SetBool("Attack", true);//"Attack"수정 필요
-
+        //Move to HQ
+        target = GameObject.Find("HQ").transform.position; Debug.Log("target" + target);  
+        force = target - gameObject.transform.position; Debug.Log("thisPos" + gameObject.transform.position); Debug.Log("force"+force); 
+        force.Normalize();
+        Debug.Log("normalize"+force);
+        force = new Vector3(force.x * speedPoint, force.y * speedPoint, force.z * speedPoint);
+        Debug.Log("multSpeed"+force);
+        GetComponent<Rigidbody>().velocity = force;
+        //GetComponent<Animator>().SetBool("Run", true);
     }
 
+    void MonsterAttack () {
+        //attack target
+        //GetComponent<Animator>().SetBool ("Attack", true);//"Attack"수정 필요
+    }
+
+    private void OnCollisionEnter (Collision collision) {
+        if(collision.gameObject.tag == "") {
+
+        }
+    }
     void CheckDeath () {
         if (healhPoint <= 0) {
-            GetComponent<Animator>().SetBool("Death", true);//"Death" 수정 필요
+            //GetComponent<Animator>().SetBool("Death", true);//"Death" 수정 필요
             StartCoroutine("DestroyDelay");
             Destroy(gameObject);
         }
