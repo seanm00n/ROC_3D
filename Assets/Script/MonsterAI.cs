39ad7bd8d94e489403d8be48064bcdc0ce9e7144
seanm00n <deccj97@naver.com>
2022-07-07 22:37:39 +0900

    fix MonsterAI

--- Assets/Script/MonsterAI.cs
+++ Assets/Script/MonsterAI.cs
@@ -1,68 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterAI : MonoBehaviour
 {
     //Do not change values with inspector
-    int healhPoint = 1;
-    int attackPoint = 1;
-    float speedPoint = 100;
-    int abilityConst = 2; //Ability constant per stage
-    Vector3 target;
-    Vector3 force;
-    public int stage = 1;
+    int stage = 3; //Get value on Instantiate
+    float health;
+    float attack;
     public bool isBoss = false;
-
+    GameObject target;
+    NavMeshAgent agent;
+    [SerializeField] //?
+    public LayerMask neutral;
 
     void Start(){
-        InitMonster();
+        Init();
     }
 
     void Update(){
-        //CheckDeath();
-        MonsterBehavior();
-        //rotate to target
-        //avoid obstacles
+        CheckDeath();
+        SelectTarget();
+        Move();
     }
 
-    void InitMonster () {
-        healhPoint = stage * abilityConst;
-        attackPoint = stage * abilityConst;
-        speedPoint = stage * abilityConst;
+    void Init () {
+        target = GameObject.Find("HQ");
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = stage * 1.6f;
+        health = stage * 1.4f;
+        attack = stage * 1.5f;
     }
 
-    void MonsterBehavior () {
-        //Move to HQ
-        target = GameObject.Find("HQ").transform.position; Debug.Log("target" + target);  
-        force = target - gameObject.transform.position; Debug.Log("thisPos" + gameObject.transform.position); Debug.Log("force"+force); 
-        force.Normalize();
-        Debug.Log("normalize"+force);
-        force = new Vector3(force.x * speedPoint, force.y * speedPoint, force.z * speedPoint);
-        Debug.Log("multSpeed"+force);
-        GetComponent<Rigidbody>().velocity = force;
-        //GetComponent<Animator>().SetBool("Run", true);
+    void Move () {
+        agent.SetDestination(target.transform.position);
     }
 
-    void MonsterAttack () {
-        //attack target
-        //GetComponent<Animator>().SetBool ("Attack", true);//"Attack"수정 필요
-    }
-
-    private void OnCollisionEnter (Collision collision) {
-        if(collision.gameObject.tag == "") {
-
-        }
-    }
     void CheckDeath () {
-        if (healhPoint <= 0) {
-            //GetComponent<Animator>().SetBool("Death", true);//"Death" 수정 필요
+        if (health <= 0) {
+            //GetComponent<Animator>().SetBool("Death", true);
             StartCoroutine("DestroyDelay");
             Destroy(gameObject);
         }
     }
-
-    IEnumerator DestroyDelay () {
-        yield return new WaitForSeconds(3f);
+    void SelectTarget () {
+        if (true) {
+            target = GameObject.Find("HQ");
+        }
+        IEnumerator DestroyDelay () {
+            yield return new WaitForSeconds(3f);
+        }
     }
 }
