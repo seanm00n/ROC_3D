fb48f5f6ef4e2bdf24ee8d92ca4d824adffea4f3
seanm00n <deccj97@naver.com>
2022-07-08 21:15:20 +0900

    feat MonsterAI 0708
    steering, priority
    
    need to fix raycast

--- Assets/Script/MonsterAI.cs
+++ Assets/Script/MonsterAI.cs
@@ -6,15 +6,17 @@ using UnityEngine.AI;
 public class MonsterAI : MonoBehaviour
 {
     //Do not change values with inspector
-    int stage = 3; //Get value on Instantiate
-    float health;
-    float attack;
-    public bool isBoss = false;
-    GameObject target;
-    NavMeshAgent agent;
-    [SerializeField] //?
-    public LayerMask neutral;
-
+    int m_stage = 3; //Get value on Instantiate
+    float m_health;
+    float m_attack;
+    bool m_isBoss = false;
+    bool m_isInRange = false;
+    GameObject m_target;
+    NavMeshAgent m_agent;
+    [SerializeField] GameObject HQ;
+    [SerializeField] GameObject Player;
+    [SerializeField] LayerMask Alliance;
+    [SerializeField] float m_SightDistance = 0f;
     void Start(){
         Init();
     }
@@ -23,33 +25,85 @@ public class MonsterAI : MonoBehaviour
         CheckDeath();
         SelectTarget();
         Move();
+        Debug.Log("Target : " + m_target);
     }
 
     void Init () {
-        target = GameObject.Find("HQ");
-        agent = GetComponent<NavMeshAgent>();
-        agent.speed = stage * 1.6f;
-        health = stage * 1.4f;
-        attack = stage * 1.5f;
+        m_target = HQ;
+        m_agent = GetComponent<NavMeshAgent>();
+        m_agent.speed = m_stage * 1.6f;
+        m_health = m_stage * 1.4f;
+        m_attack = m_stage * 1.5f;
+        if (m_isBoss) {
+            m_agent.avoidancePriority = 0;
+        } else {
+            m_agent.avoidancePriority = 1;
+        }
+    }
+
+    void SelectTarget () {
+        m_target = HQ;
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, m_SightDistance)) {
+            if (hit.transform.name == "Player") {
+                if (Vector3.Distance(transform.position, HQ.transform.position) <=
+                    Vector3.Distance(transform.position, Player.transform.position)) {
+                    m_target = HQ;
+                } else {
+                    m_target = Player;
+                }
+            }
+            if (hit.transform.CompareTag("Turret")) {
+                if (Vector3.Distance(transform.position, HQ.transform.position) <=
+                    Vector3.Distance(transform.position, hit.transform.position)) {
+                    m_target = HQ;
+                } else {
+                    m_target = hit.transform.gameObject;
+                }
+            }
+        }
     }
 
     void Move () {
-        agent.SetDestination(target.transform.position);
+        if (!m_isInRange) {
+            //GetComponent<Animator>().SetBool("Run", true);
+            m_agent.SetDestination(m_target.transform.position);
+        }
     }
 
+    void Attack () {
+        //GetComponent<Animator>().SetBool("Attack", true);
+    }
+
+    void GetHit (GameObject enemy) {
+        //GetComponent<Animator>().SetBool("Hit", true);
+        //enemy.GetComponent<Player>().m_attackPoint - m_health;
+    }
     void CheckDeath () {
-        if (health <= 0) {
+        if (m_health <= 0) {
             //GetComponent<Animator>().SetBool("Death", true);
             StartCoroutine("DestroyDelay");
             Destroy(gameObject);
         }
     }
-    void SelectTarget () {
-        if (true) {
-            target = GameObject.Find("HQ");
+
+    IEnumerator DestroyDelay () {
+        yield return new WaitForSeconds(3f);
+    }
+
+    private void OnCollisionEnter (Collision collision) {//change to other method
+        if (collision.gameObject == m_target) {
+            m_isInRange = true;
+            Attack();
+            Debug.Log("enter");
         }
-        IEnumerator DestroyDelay () {
-            yield return new WaitForSeconds(3f);
+        
+    }
+
+    private void OnCollisionExit (Collision collision) {
+        if (collision.gameObject == m_target) {
+            m_isInRange = false;
+            Debug.Log("exit");
         }
     }
 }
+
