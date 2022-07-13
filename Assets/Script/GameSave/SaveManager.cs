856e605c536dc00d9fbdc474abb73aaa4ed55409
ibelieveinrustsuperiority <20221022@vision.hoseo.edu>
2022-07-14 00:30:08 +0900

    feat: create SaveManager for managing saves
    refactor: SaveTest now uses SaveManager

---
+++ Assets/Script/GameSave/SaveManager.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace ROC
+{
+	/// <summary>
+	/// Manager class for load/saving save files.
+	/// </summary>
+	internal static class SaveManager
+	{
+		/// <summary>
+		/// Save an instance as a JSON file.
+		/// </summary>
+		/// <param name="path">Path to the file.</param>
+		/// <param name="content">Instance of <see cref="object"/> to convert into JSON format.</param>
+		internal static void Save(string path, object content)
+		{
+			var fileContent = JsonUtility.ToJson(content);
+			File.WriteAllText(Path.Combine(Application.dataPath, path), fileContent);
+		}
+
+		/// <summary>
+		/// Load a JSON file content as an instance of <see cref="object"/>.
+		/// </summary>
+		/// <param name="path">Path to the file.</param>
+		/// <typeparam name="T">The type of object you want to convert and receive from JSON content.</typeparam>
+		/// <returns>An instance of type <see cref="T"/> from JSON file content.</returns>
+		internal static T Load<T>(string path)
+		{
+			var fileContent = File.ReadAllText(Path.Combine(Application.dataPath, path));
+			return JsonUtility.FromJson<T>(fileContent);
+		}
+	}
+}
--- Assets/Script/SaveTest.cs
+++ Assets/Script/SaveTest.cs
@@ -1,51 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using ROC;
 
 [System.Serializable]
 public class Data
 {
     public List<int> userStructureData = new List<int>();
     public List<Transform> structureTransform = new List<Transform>();
-    public void LoadData()
-    {
-        Debug.Log("Start!");
-        for (int i = 0; i < userStructureData.Count; i++)
-        {
-            Debug.Log(userStructureData[i]);
-            Debug.Log(structureTransform[i]);
-        }
-
-    }
-
 }
+
 public class SaveTest : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-
-    // Update is called once per frame
-    void Start()
+    private Data data = new();
+    
+    private void LoadSaveContent(Data loadedContent)
     {
-        //Data data = new Data();
-        //data.userStructureData.Add(1);
-        //data.structureTransform.Add(transform);
-
-        //string str = JsonUtility.ToJson(data);
-
-        //File.WriteAllText(Application.dataPath + "/TestJson.json", str);
-
-        try
+        Debug.Log("LoadSaveContent() called!");
+        for (int i = 0; i < loadedContent.userStructureData.Count; i++)
         {
-            string str2 = File.ReadAllText(Application.dataPath + "/TestJson.json");
-            Data data3 = JsonUtility.FromJson<Data>(str2);
-            data3.LoadData();
-        }
-        catch (FileNotFoundException)
-        {
-            Debug.Log("Error");
+            Debug.Log(loadedContent.userStructureData[i]);
+            Debug.Log(loadedContent.structureTransform[i]);
         }
     }
-        
+    
+    private void Start()
+    {
+        Debug.Log("Press S for save, L for load");
+    }
+
+    private void Update()
+    {
+        // Save file
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            try
+            {
+                SaveManager.Save("TestJson.json", data);
+                LoadSaveContent(data);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+
+        // Load file
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            try
+            {
+                var loadedContent = SaveManager.Load<Data>("TestJson.json");
+                LoadSaveContent(loadedContent);
+
+                data = loadedContent;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+    }
 }
