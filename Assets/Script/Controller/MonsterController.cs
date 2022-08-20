96eb95ae7ec58ef5600f43eec8518f6f85681b10
Sean Moon <deccj97@naver.com>
2022-08-20 23:22:20 +0900

    add StartPosition
    add
     - Start Position 03 ~ 06

--- Assets/Script/Controller/MonsterController.cs
+++ Assets/Script/Controller/MonsterController.cs
@@ -4,27 +4,20 @@ using UnityEngine;
 using ROC;
 
 public class MonsterController : MonoBehaviour {
+
     [SerializeField] GameObject[] MonsterPref;
     [SerializeField] GameObject[] BossMonsterPref;
     [SerializeField] GameObject BoxMonsterPref;
     [SerializeField] GameObject BoxPref;
     [SerializeField] GameObject ItemPref;
-    [SerializeField] Transform StartPos01;
-    [SerializeField] Transform StartPos02;
+    [SerializeField] Transform[] StartPosTrans;
 
     GameObject[] AddsMonster;
     GameObject[] BossMonster;
-    Transform StartPos;
-    public int CurrentMonsters = 0;//MonsterAI control this value
+    Transform[] StartPos;
     PlayerSaveData data;//설정해주어야함
-    float aTimer = 0f;//1m
-    float bTimer = 0f;//5m
-    float xTimer = 0f;//50s
-    float monGenCool = 0f;
-    int MaxMonsters = 60;
-    int Aindex = 0;
-    public int Bindex = 0;
-    bool flag;
+    public int CurrentMonsters = 0;//MonsterAI control this value
+    int WaveNum = 0;
     
     /// 코드 수정함 (변경자 : zin)
     public GameObject endPos;
@@ -34,69 +27,44 @@ public class MonsterController : MonoBehaviour {
         Init(); 
     }
     void Update () {
-        AddsMonsterGen();
         BossMonsterGen();
         BoxMonsterGen();
+        CountCurrentMonsters();
     }
 
     void Init () {
-        AddsMonster = new GameObject[21];
-        for(int index01 = 0; index01 < 21; index01++) {
+        AddsMonster = new GameObject[20];
+        for(int index01 = 0; index01 < 20; index01++) {
             AddsMonster[index01] = MonsterPref[index01];
         }
         BossMonster = new GameObject[4];
         for (int index02 = 0; index02 < 4; index02++) {
             BossMonster[index02] = BossMonsterPref[index02];
         }
-        flag = false;
-        xTimer = 50;
+        for (int index03 = 0; index03 < 6; index03++)
+        {
+            StartPos[index03] = StartPosTrans[index03];
+        }
         CurrentMonsters = 0;
     }
+    void CountCurrentMonsters()
+    {
+        if(0 < CurrentMonsters) AddsMonsterGen();
+    }
     void AddsMonsterGen () {
-        //잡몹 프리펩이 1분마다 변경됨, 20분 부터 함수 실행 안됨
-        if (21 <= Aindex) return;
-        aTimer += Time.deltaTime;
-        monGenCool += Time.deltaTime;
-        if (60f < aTimer) {
-            aTimer = 0f;
-            Aindex++;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                Instantiate(AddsMonster[WaveNum], StartPos[j].position, StartPos[j].rotation);
+            }
         }
-        //몬스터 수 비교해 계속 소환
-        
-        if (CurrentMonsters < MaxMonsters && 0.3f < monGenCool) {
-            monGenCool = 0f;
-            StartPos = flag ? StartPos01 : StartPos02;
-            Instantiate(AddsMonster[Aindex],StartPos.position,StartPos.rotation);
-            CurrentMonsters++;
-            flag = flag ? false : true ;
-        }
+        CurrentMonsters = 30;
+        WaveNum++;
     }
     void BoxMonsterGen() {
-        xTimer += Time.deltaTime;
-        if (50f < xTimer) {
-            xTimer = 0f;
-            StartPos = flag ? StartPos01 : StartPos02;
-            Instantiate(BoxMonsterPref, StartPos.position, StartPos.rotation);
-            flag = flag ? false : true;
-        }
+
     }
     void BossMonsterGen () {
-        //1분마다 보스몹 젠, 20분 부터 함수 실행 안됨
-        if (4 <= Bindex) return;
-        bTimer += Time.deltaTime;
-        if (300f < bTimer) { 
-            StartPos = flag ? StartPos01 : StartPos02;
 
-            /// 코드 수정함 (변경자 : zin) 드래곤 위치 받기
-            if (Bindex == 3)
-            {
-                endPos = Instantiate(BossMonster[Bindex], StartPos.position, StartPos.rotation);
-            }
-            else
-            Instantiate(BossMonster[Bindex], StartPos.position, StartPos.rotation);
-            bTimer = 0f;
-            Bindex++;
-        }
     }
     public void GameClear() 
     {
